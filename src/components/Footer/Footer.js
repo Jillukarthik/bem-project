48671dcd45fb3a28e0b6a3f8e4605f7d2363ecf5
Jillukarthik <2001karthicksrini@gmail.com>
2023-03-06 23:15:33 +0530

    buileded

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,18 +2,22 @@ import React from "react";
 import "./Footer.css";
 import { Box, Flex, Spacer } from "@chakra-ui/layout";
 import { TriangleUpIcon } from "@chakra-ui/icons";
+
+
 function Footer() {
   return (
     <Box className="footer">
       <Flex className="footer__items">
+      <Spacer />
         <Box className="footer__item">Privacy Policy | Term of use</Box>
         <Spacer />
         <Box className="footer__item">
           {" "}
-          <TriangleUpIcon /> | This website was designed with XXY.
+          <TriangleUpIcon /> | TECH STACK
         </Box>
         <Spacer />
-        <Box className="footer__item">© 2006 - 2023 XXY.com, Inc</Box>
+        <Box className="footer__item">© 2006 - 2023 TECHSTACK.com, Inc</Box>
+        <Spacer/>
       </Flex>
     </Box>
   );
